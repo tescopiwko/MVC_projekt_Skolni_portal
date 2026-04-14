530a99a72a80122c71bdb37cd5c5093ea6ac2d83
Jan Erlebach <erlebachj24@spstrutnov.cz>
2026-04-14 10:13:09 +0200

    Slouceni projektu s Github repozitarem

---
+++ MVC_projekt_Skolni_portal/MVC_projekt_Skolni_portal/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using MVC_projekt_Skolni_portal.Models;
+using System.Diagnostics;
+
+namespace MVC_projekt_Skolni_portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
