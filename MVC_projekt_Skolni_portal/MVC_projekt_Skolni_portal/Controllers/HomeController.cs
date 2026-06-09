ac538d6f31e50943879dd01086beb02866d14f46
Petr Bártl <bartlp24@spstrutnov.cz>
2026-06-09 10:26:15 +0200

    Udělání stránky více
    button zjistit více už není k ničemu

--- MVC_projekt_Skolni_portal/MVC_projekt_Skolni_portal/Controllers/HomeController.cs
+++ MVC_projekt_Skolni_portal/MVC_projekt_Skolni_portal/Controllers/HomeController.cs
@@ -21,6 +21,11 @@ namespace MVC_projekt_Skolni_portal.Controllers
             return View();
         }
 
+        public IActionResult Vice()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
